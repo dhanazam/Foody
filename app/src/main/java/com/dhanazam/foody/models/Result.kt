f5df418d6d98ded1818c04658979fba98cb02a56
Ramadhan <ramadanazam93@gmail.com>
2024-03-31 02:31:32 +0800

    add data models

---
+++ app/src/main/java/com/dhanazam/foody/models/Result.kt
@@ -0,0 +1,37 @@
+package com.dhanazam.foody.models
+
+import com.dhanazam.foody.models.ExtendedIngredient
+import com.google.gson.annotations.SerializedName
+
+data class Result(
+    @SerializedName("aggregateLikes")
+    val aggregateLikes: Int,
+    @SerializedName("cheap")
+    val cheap: Boolean,
+    @SerializedName("diaryFree")
+    val dairyFree: Boolean,
+    @SerializedName("extendedIngredients")
+    val extendedIngredient: List<ExtendedIngredient>,
+    @SerializedName("glutenFree")
+    val glutenFree: Boolean,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("readyInMinutes")
+    val readyInMinutes: Int,
+    @SerializedName("sourceName")
+    val sourceName: String,
+    @SerializedName("sourceUrl")
+    val sourceUrl: String,
+    @SerializedName("summary")
+    val summary: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("vegan")
+    val vegan: Boolean,
+    @SerializedName("vegetarian")
+    val vegetarian: Boolean,
+    @SerializedName("veryHealthy")
+    val veryHealthy: Boolean
+)
