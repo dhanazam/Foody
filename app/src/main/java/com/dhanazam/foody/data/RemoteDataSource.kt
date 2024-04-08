37f2111123bdcb57af561bbe29ce33e79f23bb3e
Ramadhan <ramadanazam93@gmail.com>
2024-04-09 00:03:12 +0800

    create networkResult class

---
+++ app/src/main/java/com/dhanazam/foody/data/RemoteDataSource.kt
@@ -0,0 +1,15 @@
+package com.dhanazam.foody.data
+
+import com.dhanazam.foody.data.network.FoodRecipesApi
+import com.dhanazam.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor (
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+}
