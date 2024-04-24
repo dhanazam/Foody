ab1dea17a4c14a80106c0beb65eaf82f783ea4a9
Ramadhan <ramadanazam93@gmail.com>
2024-04-24 11:52:34 +0800

    add DAO recipes

---
+++ app/src/main/java/com/dhanazam/foody/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.dhanazam.foody
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.dhanazam.foody.models.FoodRecipe
+import com.dhanazam.foody.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    val foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
