3a45d0e70552fed7900819c319eb1605a6b9f8b6
dhanazam <ramadanazam93@gmail.com>
2025-02-08 22:08:51 +0800

    design activity details layout

---
+++ app/src/main/java/com/dhanazam/foody/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.dhanazam.foody.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dhanazam.foody.R
+
+class DetailsActivity : AppCompatActivity() {
+
+}
