29746eb6d4faf390105428bb8cb867e85c1d7cab
dhanazam <ramadanazam93@gmail.com>
2025-04-22 15:33:07 +0800

    design fragment overview & ingredients row layout

--- app/src/main/java/com/dhanazam/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/dhanazam/foody/bindingadapters/RecipesRowBinding.kt
@@ -12,6 +12,7 @@ import coil.load
 import com.dhanazam.foody.R
 import com.dhanazam.foody.models.Result
 import com.dhanazam.foody.ui.fragments.recipes.RecipesFragmentDirections
+import org.jsoup.Jsoup
 
 class RecipesRowBinding {
 
@@ -73,6 +74,15 @@ class RecipesRowBinding {
                         )
                     }
                 }
+            }
+        }
+
+        @BindingAdapter("parseHtml")
+        @JvmStatic
+        fun parseHtml(textView: TextView, description: String?) {
+            if (description != null) {
+                val desc = Jsoup.parse(description).text()
+                textView.text = desc
             }
         }
     }
--- app/src/main/java/com/dhanazam/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/dhanazam/foody/ui/DetailsActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.dhanazam.foody.R
+import com.dhanazam.foody.adapter.PagerAdapter
 import com.dhanazam.foody.databinding.ActivityDetailsBinding
+import com.dhanazam.foody.ui.fragments.ingredients.IngredientsFragment
+import com.dhanazam.foody.ui.fragments.instructions.InstructionsFragment
+import com.dhanazam.foody.ui.fragments.overview.OverviewFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailsActivity : AppCompatActivity() {
 
+    private val args by navArgs<DetailsActivityArgs>()
     private lateinit var binding: ActivityDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +27,34 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredient")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val pagerAdapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            this
+        )
+
+        binding.viewPager2.isUserInputEnabled = false
+        binding.viewPager2.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+            tab.text = titles[position]
+        }.attach()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
